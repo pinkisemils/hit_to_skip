8669edd1d6987d96301dba1e1d8cf3658bc8aa80
Emīls Piņķis <pinkisemils@gmail.com>
2017-02-12 03:32:11 +0000

    Fixed the ranks table to reference the users by a varchar and not an integer

--- migrations/20170212025328_ranks/up.sql
+++ migrations/20170212025328_ranks/up.sql
@@ -1,7 +1,7 @@
 -- no songs are bad, there are just less upvoted songs
 CREATE TABLE ranks (
     song_id INTEGER REFERENCES tracks (id),
-    user_id INTEGER REFERENCES users (id),
+    user_id VARCHAR REFERENCES users (id),
     timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY_KEY(song_id, user_id, timestamp),
 )
