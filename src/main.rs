8f4cf5bc303198da114f05b6e0d4487a21a5835f
Emīls Piņķis <pinkisemils@gmail.com>
2017-02-12 11:27:02 +0000

    $ git commit -m "sepukku"

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::time;
 use std::io::prelude::*;
 use serial::prelude::*;
 
-//fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
+// fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
 //    try!(port.reconfigure(&|settings| {
 //        try!(settings.set_baud_rate(serial::Baud9600));
 //        settings.set_char_size(serial::Bits8);
@@ -29,43 +29,48 @@ use serial::prelude::*;
 //    try!(port.read(&mut buf[..]));
 //
 //    Ok(())
-//}
+// }
 
 fn read_serial(path: String, tx: mpsc::Sender<PlayState>) {
     let mut port = serial::open(&path).expect("What the heckin' heck, can't even open up a TTY");
     port.reconfigure(&|settings| {
-        settings.set_baud_rate(serial::Baud9600).expect("Couldn't even set the baud rate");
-        settings.set_char_size(serial::Bits8);
-        settings.set_parity(serial::ParityNone);
-        settings.set_stop_bits(serial::Stop1);
-        settings.set_flow_control(serial::FlowNone);
-        Ok(())
-    }).expect("Couldn't even configure the damn tty");
-    port.set_timeout(time::Duration::from_millis(1000)).expect("Couldn't even set the timeout");
+            settings.set_baud_rate(serial::Baud9600).expect("Couldn't even set the baud rate");
+            settings.set_char_size(serial::Bits8);
+            settings.set_parity(serial::ParityNone);
+            settings.set_stop_bits(serial::Stop1);
+            settings.set_flow_control(serial::FlowNone);
+            Ok(())
+        })
+        .expect("Couldn't even configure the damn tty");
+    port.set_timeout(time::Duration::from_millis(10 * 1000)).expect("Couldn't even set the timeout");
     loop {
         let mut input_buf = vec![0x0u8];
-        if let Ok(read) = port.read(&mut input_buf) {
-            match input_buf[0] as char {
-                's' => {
-                    match tx.send(PlayState::Skip) {
-                        Ok(_) => continue,
-                        Err(_) => return,
+        match port.read(&mut input_buf) {
+            Ok(_) => {
+                match input_buf[0] as char {
+                    's' => {
+                        match tx.send(PlayState::Skip) {
+                            Ok(_) => continue,
+                            Err(e) => return,
+                        }
                     }
-                },
-                _ =>continue,
+                    _ => continue,
+                }
+            }
+            Err(e) => {
+                println!("Encountered e: {}", e);
+                println!("It's time to stop");
+                return;
 
             }
 
-        } else {
-            println!("It's time to stop");
-            return;
         }
     }
 
 }
 
 
-enum PlayState{
+enum PlayState {
     PauseResume,
     Skip,
     Stop,
@@ -78,21 +83,21 @@ fn simple_example(file_path: &Path, rx: &mpsc::Receiver<PlayState>) -> bool {
 
         // set option "sid" to "no" (no subtitles)
         // mpv options should be set before initializing
-        mpv_builder.set_option("sid","no").unwrap();
+        mpv_builder.set_option("sid", "no").unwrap();
 
         let mut mpv = mpv_builder.build().expect("Failed to build MPV handler");
 
         mpv.command(&["loadfile", file_path as &str])
-           .expect("Error loading file");
+            .expect("Error loading file");
 
         // loop twice, send parameter as a string
 
         // set speed to 100%, send parameter as a f64
-        mpv.set_property("speed",1.0).unwrap();
+        mpv.set_property("speed", 1.0).unwrap();
 
         // get how many loops are playing as an i64
-        //sleep_ms(3000);
-        //mpv.set_property_async("pause", true, 1).expect("Failed to pause player");
+        // sleep_ms(3000);
+        // mpv.set_property_async("pause", true, 1).expect("Failed to pause player");
 
         let mut playing = true;
         let mut stop = false;
@@ -103,7 +108,7 @@ fn simple_example(file_path: &Path, rx: &mpsc::Receiver<PlayState>) -> bool {
                 match event {
                     // Shutdown will be triggered when the window is explicitely closed,
                     // while Idle will be triggered when the queue will end
-                    //mpv::Event::Shutdown | mpv::Event::Idle => {
+                    // mpv::Event::Shutdown | mpv::Event::Idle => {
                     mpv::Event::EndFile(_) => {
                         stop = false;
                         break 'main;
@@ -122,7 +127,7 @@ fn simple_example(file_path: &Path, rx: &mpsc::Receiver<PlayState>) -> bool {
                         PlayState::PauseResume => {
                             mpv.set_property("pause", playing).expect("Failed to pause/resume");
                             playing = !playing;
-                        },
+                        }
                         PlayState::Skip => {
                             break;
                         }
@@ -134,7 +139,7 @@ fn simple_example(file_path: &Path, rx: &mpsc::Receiver<PlayState>) -> bool {
                 }
             };
         }
-        match mpv.command(&["quit", "0"]){
+        match mpv.command(&["quit", "0"]) {
             Err(e) => println!("Failed to quit current mpv instance: {}", e),
             _ => (),
         }
@@ -149,8 +154,8 @@ fn simple_example(file_path: &Path, rx: &mpsc::Receiver<PlayState>) -> bool {
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let (mpv_major_api_v,mpv_minor_api_v) = mpv::client_api_version();
-    println!("MPV_API_VERSION : v{}.{}",mpv_major_api_v,mpv_minor_api_v);
+    let (mpv_major_api_v, mpv_minor_api_v) = mpv::client_api_version();
+    println!("MPV_API_VERSION : v{}.{}", mpv_major_api_v, mpv_minor_api_v);
     if args.len() < 2 {
         println!("Usage: ./simple [any mp4, avi, mkv, ... file]");
     } else {
@@ -172,9 +177,7 @@ fn main() {
         });
 
         let s_tx = tx.clone();
-        let s_handle = spawn(||{
-            read_serial("/dev/ttyACM0".to_string(), s_tx)
-        });
+        let s_handle = spawn(|| read_serial("/dev/ttyACM0".to_string(), s_tx));
         // code that will wait for logic shit to come in and tell if it should skip or not
         loop {
             let mut input = String::new();
@@ -183,20 +186,25 @@ fn main() {
                     let input = input.trim();
                     match input.as_ref() {
                         "q" => {
-                            tx.send(PlayState::Stop).expect("failed to send command to player chan");
+                            tx.send(PlayState::Stop)
+                                .expect("failed to send command to player chan");
                             break;
-                        },
-                        "p" => tx.send(PlayState::PauseResume).expect("failed to send command to player chan"),
+                        }
+                        "p" => {
+                            tx.send(PlayState::PauseResume)
+                                .expect("failed to send command to player chan")
+                        }
                         _ => {
                             println!("Received {:?}", input);
-                            println!("Expected commands are \n s - skip\n q - quit\n p - pause/play\n");
+                            println!("Expected commands are \n s - skip\n q - quit\n p - \
+                                      pause/play\n");
                         }
                     }
                 }
                 Err(e) => {
                     println!("Failed to read stdin because {}", e);
                     break;
-                },
+                }
 
             }
 
