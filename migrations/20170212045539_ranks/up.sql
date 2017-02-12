2840599760d07d607b9294610ef16587d6316a90
Emīls Piņķis <pinkisemils@gmail.com>
2017-02-12 04:57:46 +0000

    Fixed schema once more

--- migrations/20170212045539_ranks/up.sql
+++ migrations/20170212045539_ranks/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ranks (
     song_id INTEGER REFERENCES tracks (id),
     user_id VARCHAR REFERENCES users (user_id),
-    timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY(song_id, user_id, timestamp)
 )
